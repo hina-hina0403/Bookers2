bee7bb9545ebfc2f03a97eb44174b9bee75d7779
EC2 Default User <ec2-user@ip-172-31-25-137.ap-northeast-1.compute.internal>
2024-06-02 12:22:48 +0000

    [Add] フラッシュメッセージ

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,7 @@ class User < ApplicationRecord
   has_one_attached :profile_image
 
   validates :name, presence: true
-  validates :introduction, presence: true
-  validates :profile_image, presence: true
+  # validates :introduction, presence: true
 
   def get_profile_image(width,height)
     unless profile_image.attached?
