13ec9970871fde98ce154338c753d46923706b55
EC2 Default User <ec2-user@ip-172-31-25-137.ap-northeast-1.compute.internal>
2024-06-02 11:24:37 +0000

    [Add] プロフィール更新失敗

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :books, dependent: :destroy
-
-
   has_one_attached :profile_image
+
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :profile_image, presence: true
 
   def get_profile_image(width,height)
     unless profile_image.attached?
